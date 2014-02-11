539f75d2ad3253c1c8d0dde743cf174e925381fd
Vivek Singh <vivek@ubuntu.(none)>
2014-02-11 20:06:07 +0530

    Updated from Vamika Admin

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-ChequeMaker::Application.config.session_store :cookie_store, key: '_vamikaadmin_session'
+ChequeMaker::Application.config.session_store :cookie_store, key: '_chequemaker_session'
