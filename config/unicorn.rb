54bf08311bd01382fb451cec5c78d2f8a710e46a
Vivek Kumar <viveksingh0143@gmail.com>
2014-02-23 17:01:13 +0530

    Deployment Config File Changed

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,9 +1,9 @@
 root = "/home/vamika/apps/chequemaker/current"
 working_directory root
-pid "#{root}/tmp/pids/unicorn.pid"
+pid "#{root}/tmp/pids/unicorn_chequemaker.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen "/tmp/unicorn.blog.sock"
+listen "/tmp/unicorn.chequemaker.sock"
 worker_processes 2
 timeout 30
--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -4,7 +4,7 @@ set -e
 # Feel free to change any of the following variables for your app:
 TIMEOUT=${TIMEOUT-60}
 APP_ROOT=/home/vamika/apps/chequemaker/current
-PID=$APP_ROOT/tmp/pids/unicorn.pid
+PID=$APP_ROOT/tmp/pids/unicorn_chequemaker.pid
 CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
 AS_USER=vamika
 set -u
